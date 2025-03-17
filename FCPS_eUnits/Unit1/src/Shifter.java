0580590cc235b022a51c7d38a323ac86cf177cf5
morris940216 <morris940216@gmail.com>
2025-03-17 16:12:08 +0800

    0317OK
    ok

--- FCPS_eUnits/Unit1/src/Shifter.java
+++ FCPS_eUnits/Unit1/src/Shifter.java
@@ -1,4 +1,4 @@
-	//Name______________________________ Date_____________
+ 	//Name______________________________ Date_____________
    import edu.fcps.karel2.Display;
    import edu.fcps.karel2.Robot;
    public class Shifter extends Robot implements Runnable, Workable
@@ -9,9 +9,33 @@
          super(1, y, Display.EAST, 0);
       
       }
-   	/*********************************
-   	 *                               *
-   	 * What methods must you define? *
-   	 *                               *
-   	 *********************************/
-   }
+   	public void workCorner()
+      {
+      
+      }
+      public void moveOneBlock()
+      {
+         
+      }
+      public void turnToTheRight()
+      {
+         
+      }
+      public void turnToTheNorth()
+      {
+         }
+      public void run()
+      {
+       int[] counterAry = new int[10];
+       for(int i=0;i<counterAry.length;i++){
+            while(nextToABeeper()){
+               pickBeeper();
+               counterAry[i]++;
+               }
+               if(i>0){
+               for(int j = 0; j<counterAry[i-1];j++){
+                  putBeeper();
+                               }}
+               move();
+
+   }}}
