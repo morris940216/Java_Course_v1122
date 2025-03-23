1b21de8b5fc9e82549527894fd41018cca055569
morris940216 <morris940216@gmail.com>
2025-03-23 22:36:25 +0800

    lab17

---
+++ FCPS_eUnits/Unit1/src/Democrat.java
@@ -0,0 +1,19 @@
+
+    public class Democrat extends MazeEscaper
+   {
+       public void walkDownCurrentSegment()
+      {
+         if(!leftIsClear()&& frontIsClear()&& !nextToABeeper()){
+            move();
+            }
+      }
+       public void turnToTheNextSegment()
+      {  
+         if(leftIsClear()){
+            turnLeft();
+            move();}
+         else{
+            turnRight();}
+               }
+         
+   }
---
+++ FCPS_eUnits/Unit1/src/Republican.java
@@ -0,0 +1,19 @@
+
+    public class Republican extends MazeEscaper
+   {
+       public void walkDownCurrentSegment()
+      {
+         if(!rightIsClear()&& frontIsClear()&& !nextToABeeper()){
+            move();
+            }
+      }
+       public void turnToTheNextSegment()
+      {  
+         if(rightIsClear()){
+            turnRight();
+            move();}
+         else{
+            turnLeft();}
+               }
+         
+   }
